afff9160ed9d96e5f6d26e67814d383a4ff4765e
Felipe <alunopython1@gmail.com>
2025-11-28 15:02:21 -0300

    Add arithmetic operations routes to Flask app

--- app_flask.py
+++ app_flask.py
@@ -7,7 +7,39 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-## Continue o código aqui.
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/soma")
+def soma():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    return {"resultado":v1 + v2}
+
+@app.route("/subtrair")
+def subtrair():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    return {"resultado":v1 - v2}
+
+@app.route("/multiplicar")
+def multiplicar():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    return {"resultado":v1 * v2}
+
+@app.route("/dividir")
+def dividir():
+    v1 = float(request.args.get("valor1", 0))
+    v2 = float(request.args.get("valor2", 0))
+    if v2 == 0:
+        return {'Erro':'Por favor coloque um número divísivel!' }
+    return {"resultado":v1 / v2}
 
 if __name__ == "__main__":
     app.run(debug=True)
